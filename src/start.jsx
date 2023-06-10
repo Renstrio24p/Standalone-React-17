c1b0aec2c551e09c545c48b58031768ddee26df7
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-06-10 00:04:16 +0000

    deploy: f7c08bbc2bb7e3299248a03de6efbc9fd9593ba2

---
+++ src/start.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Join } from "./assets/util/JoinClasses";
+import styles from './assets/sass/modules/app.module.scss'
+
+export default function Start(){
+
+    React.useEffect(()=>{
+        document.title = 'Start React'
+    })
+
+    return (
+        <div>
+            <h1 className={Join([styles['dark-blue'],styles.weight,styles.pad])}>
+                Hello <span className={Join([styles.weight,styles.black])}>React 17</span>
+                <img src="/images/react.png" alt="react-logo" />
+            </h1>
+            {/* You can now start coding right here..*/}
+        </div>
+    )
+}
+// this will serve as the APP.JS 
