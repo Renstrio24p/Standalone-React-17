2bd6224ff622fa025b47ad73f0064bb450fd0ebe
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-06-10 00:18:47 +0000

    deploy: ba65df4b9d34762f0c0980ba6f989ef204ed5a0b

--- src/start.jsx
+++ src/start.jsx
@@ -12,7 +12,7 @@ export default function Start(){
         <div>
             <h1 className={Join([styles['dark-blue'],styles.weight,styles.pad])}>
                 Hello <span className={Join([styles.weight,styles.black])}>React 17</span>
-                <img src="./src/images/react.png" alt="react-logo" />
+                <img src="./src/images/react.png" alt="react-logo"/>
             </h1> 
             {/* You can now start coding right here..*/}
         </div>
