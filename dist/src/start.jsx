c51b160d696f03be790c223ad1e8ec5a5e42101d
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-10 08:09:35 +0800

    test

--- dist/src/start.jsx
+++ dist/src/start.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-// import Notes from "./projects/Medium/Note-App/Notes";
-
+import { Join } from "./assets/util/JoinClasses";
+import styles from './assets/sass/modules/app.module.scss'
 
 export default function Start(){
+
+    React.useEffect(()=>{
+        document.title = 'Start React'
+    })
+
     return (
         <div>
-            <h1>
-                Hello World  
+            <h1 className={Join([styles['dark-blue'],styles.weight,styles.pad])}>
+                Hello <span className={Join([styles.weight,styles.black])}>React 17</span>
+                <img src="/images/react.png" alt="react-logo" />
             </h1>
             {/* You can now start coding right here..*/}
         </div>
